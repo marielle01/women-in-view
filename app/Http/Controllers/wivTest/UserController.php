4f9e9a56a0bac4b3b3f9c7c927a9edc8552b1da6
Marielle K <marielle.koffi1@gmail.com>
2023-10-20 12:31:45 +0200

    add test GET

---
+++ app/Http/Controllers/wivTest/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\wivTest;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $users = UserResource::collection(User::all());
+        return $this->sendResponse($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+
+}
