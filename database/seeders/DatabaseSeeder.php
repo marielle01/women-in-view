119bebb7588ec64eaa0aee7b8e279e25be8f4dd9
Marielle K <marielle.koffi1@gmail.com>
2023-12-19 00:25:26 +0100

    Add permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\RoleEnum;
 use App\Models\TvMovie;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //User::factory(5)->create();
+        $this->call(class: RoleSeeder::class);
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // creation Users
+        $userRole = Role::firstWhere('name', RoleEnum::USER->value);
 
-        TvMovie::factory(5)->create();
+        User::factory(5)
+            ->create()
+            ->each(
+                fn(User $user) => $user->assignRole($userRole)
+            );
+
+
+        // creation Super Admin
+        User::factory()->create([
+             'name' => 'Super Admin',
+             'email' => 'superAdmin@test.com',
+        ])->assignRole(Role::firstWhere('name', RoleEnum::SUPER_ADMIN->value));
+
+
+        // creation Admins
+        $adminRole = Role::firstWhere('name', RoleEnum::ADMIN->value);
+
+        User::factory(5)
+            ->create()
+            ->each(
+                fn(User $user) => $user->assignRole($adminRole)
+            );
+
+        //TvMovie::factory(5)->create();
     }
 }
