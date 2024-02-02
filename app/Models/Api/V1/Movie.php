a9a3ef6cb3312c88ee8fa218257a60cd1e664fe9
Marielle K <marielle.koffi1@gmail.com>
2024-02-02 18:26:11 +0100

    add migration, model, controller for movies and reviews tables

---
+++ app/Models/Api/V1/Movie.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Api\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'imdbId',
+        'title',
+        'image',
+        'year',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function reviews(): BelongsTo
+    {
+        return $this->belongsTo(Review::class);
+    }
+}
