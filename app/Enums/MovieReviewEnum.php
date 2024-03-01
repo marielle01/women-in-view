cc394c009e3929ff8842f7831baab66f62244c03
Marielle K <marielle.koffi1@gmail.com>
2024-03-01 11:56:00 +0100

    clean code and add moviesSeeder

--- app/Enums/MovieReviewEnum.php
+++ app/Enums/MovieReviewEnum.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 
 enum MovieReviewEnum: int
 {
-    case TEST_OK = 2;
+    case TEST_OK = 3;
     case TEST_WRONG = 1;
     case NO_TEST = 0;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,45 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //User::factory(5)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // TvMovie::factory(5)->create();
-
-        /*$this->call(class: RoleSeeder::class);
-
-        // creation Editor
-        $userRole = Role::firstWhere('name', RoleEnum::EDITOR->value);
-
-        User::factory(5)
-            ->create()
-            ->each(
-                fn(User $user) => $user->assignRole($userRole)
-            );
-
-
-        // creation Super Admin
-        User::factory()->create([
-            'name' => 'Super Admin',
-            'email' => 'superAdmin@test.com',
-        ])->assignRole(Role::firstWhere('name', RoleEnum::SUPER_ADMIN->value));
-
-
-        // creation Admins
-        $adminRole = Role::firstWhere('name', RoleEnum::ADMIN->value);
-
-        User::factory(2)
-            ->create()
-            ->each(
-                fn(User $user) => $user->assignRole($adminRole)
-            );*/
-
-        $this->call(RoleSeeder::class);
-        $this->call(AdminSeeder::class);
+        $this->call([
+            MovieSeeder::class,
+        ]);
 
     }
 }
