f595dc24d15ba0cf3d382592b8fe17ecbc82cbaa
Marielle K <marielle.koffi1@gmail.com>
2024-03-21 00:46:16 +0100

    WIP: add metadata

--- app/Http/Controllers/Api/V1/MovieController.php
+++ app/Http/Controllers/Api/V1/MovieController.php
@@ -78,7 +78,49 @@ class MovieController extends BaseController
      * @return JsonResponse
      */
 
-   /** public function searchMovie(string $movie, int $perPage = 12): JsonResponse
+
+   /* public function searchMovie(string $movie, int $perPage = 12): JsonResponse
+    {
+        // search all movies whose original title starts with that match with wildcard character
+        $search1 = Movie::where('original_title','like','%'.$movie)
+            //->paginate($perPage)
+            ->paginate(2)
+            ->keyBy('tmdb_id');
+
+        // search all movies whose original title ends with that match with wildcard character
+        $search2 = Movie::where('original_title','like',$movie.'%')
+            //->paginate($perPage)
+            ->paginate(2)
+            ->keyBy('tmdb_id');
+
+        // search all movies whose original title contains that match with wildcard character
+        $search3 = Movie::where('original_title','like','%'.$movie.'%')
+            //->paginate($perPage)
+            ->paginate(2)
+            ->keyBy('tmdb_id');
+
+        // search movies from external API
+        $movies = Http::withToken(
+            config('services.tmdb.token')
+        )->get('https://api.themoviedb.org/3/search/movie?query='.$movie)->json();
+
+        // convert results to a collection and key them by tmdb_id
+        $collection = collect($movies['results'])->keyBy('id')->map(function ($item) {
+            return array_merge($item, ['tmdb_id' => $item['id']]);
+        });
+
+        // merge search results and external API results
+        $search = $search1->merge($search2)->merge($search3)->concat($collection);
+
+        // remove duplicates based on tmdb_id
+        $search = $search->unique('tmdb_id');
+
+        // return paginated results
+        //return $this->sendResponse($search->forPage(request()->input('page', 1), $perPage));
+        return $this->sendResponse($search);
+    }*/
+
+    public function searchMovie(string $movie, int $perPage = 5): JsonResponse
     {
         // search all movies whose original title starts with that match with wildcard character
         $search1 = Movie::where('original_title','like','%'.$movie)
@@ -111,41 +153,24 @@ class MovieController extends BaseController
         // remove duplicates based on tmdb_id
         $search = $search->unique('tmdb_id');
 
-        // return paginated results
-        return $this->sendResponse($search->forPage(request()->input('page', 1), $perPage));
-    }**/
+        // get the current page
+        $currentPage = request()->input('page', 1);
 
-    public function searchMovie(string $movie, int $perPage = 2): JsonResponse
-    {
-        // Create a Spatie Query Builder instance
-        $query = QueryBuilder::for(Movie::class)
-            ->allowedFilters(['original_title'])
-            ->allowedSorts(['original_title'])
-            ->where(function ($query) use ($movie) {
-                // search all movies whose original title starts with that match with wildcard character
-                $query->where('original_title','like','%'.$movie)
-                    ->orWhere('original_title','like',$movie.'%')
-                    ->orWhere('original_title','like','%'.$movie.'%');
-            });
+        // slice the collection to get the items for the current page
+        $items = $search->slice(($currentPage - 1) * $perPage, $perPage)->values();
 
-        // search movies from external API
-        $movies = Http::withToken(
-            config('services.tmdb.token')
-        )->get('https://api.themoviedb.org/3/search/movie?query='.$movie)->json();
+        // get the total number of pages
+        $totalPages = ceil(count($search) / $perPage);
 
-        // convert results to a collection and key them by tmdb_id
-        $collection = collect($movies['results'])->keyBy('id')->map(function ($item) {
-            return array_merge($item, ['tmdb_id' => $item['id']]);
-        });
-
-        // merge search results and external API results
-        $search = $query->get()->keyBy('tmdb_id')->concat($collection);
-
-        // remove duplicates based on tmdb_id
-        $search = $search->unique('tmdb_id');
-
-        // return paginated results
-        return $this->sendResponse($search->forPage(request()->input('page', 1), $perPage));
+        // return the items, current page, total pages, and total results
+        return response()->json([
+            'data' => $items,
+            'meta' => [
+                'current_page' => $currentPage,
+                'total_pages' => $totalPages,
+                'total_results' => count($search)
+            ]
+        ]);
     }
 
 
