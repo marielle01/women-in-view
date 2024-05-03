b88b430373a93810f5dd38abcc5a323e7770b9cb
Julie <jem.baret@gmail.com>
2024-05-03 15:14:41 +0200

    chore: allow origins from frontend url and localhost only to improve security

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'https://women-in-view.vercel.app/',
+        'https://wiv.adahub.fr/',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
